bde8a217c7d17f3e11644e0611998bd4700da07f
Christoph Massmann <cm@vianetz.com>
2023-09-18 20:25:04 +0200

    fixed issue with latest php version, updated composer dependency

--- app/code/community/Meanbee/CmsPageRevisions/Block/Adminhtml/Cms/Block/Review.php
+++ app/code/community/Meanbee/CmsPageRevisions/Block/Adminhtml/Cms/Block/Review.php
@@ -1,13 +1,15 @@
 <?php
 
-class Meanbee_CmsPageRevisions_Block_Adminhtml_Cms_Block_Review extends Mage_Core_Block_Template {
-
-    public function isHtml($string) {
-        if ( $string != strip_tags($string) )
-            return true; // Contains HTML
+class Meanbee_CmsPageRevisions_Block_Adminhtml_Cms_Page_Review extends Mage_Core_Block_Template
+{
+    public function isHtml(string $string): bool
+    {
+        // Contains HTML
+        return $string !== strip_tags($string);
     }
 
-    public function getDisplayName($name) {
+    public function getDisplayName($name)
+    {
         return ucwords(str_replace('_', ' ', $name));
     }
 }
