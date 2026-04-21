0b8b895536041eb380a1560896117ba9c60630e0
Christoph Massmann <cm@vianetz.com>
2026-04-21 09:03:53 +0200

    replaced legacy Mysql4 references

--- app/code/community/Meanbee/CmsPageRevisions/Model/Mysql4/Cms/Page/Revisions/Collection.php
+++ app/code/community/Meanbee/CmsPageRevisions/Model/Mysql4/Cms/Page/Revisions/Collection.php
@@ -1,5 +1,5 @@
 <?php
-class Meanbee_CmsPageRevisions_Model_Mysql4_Cms_Page_Revisions_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract {
+class Meanbee_CmsPageRevisions_Model_Mysql4_Cms_Page_Revisions_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
 
     protected function _construct() {
         $this->_init('meanbee_cmspagerevisions/cms_page_revisions');
